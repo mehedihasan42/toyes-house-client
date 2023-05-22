8c52cca69f9c4ee7579de577ad43ca3025bc92ed
mehedi hasan <tanjildewan10@gmail.com>
2023-05-22 20:06:44 +0600

    project chekck

--- src/Pages/Layoutes/Main.jsx
+++ src/Pages/Layoutes/Main.jsx
@@ -6,7 +6,6 @@ import { Outlet } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            main swction
            <Navbar></Navbar>
            <Outlet></Outlet>
            <Footer></Footer>
