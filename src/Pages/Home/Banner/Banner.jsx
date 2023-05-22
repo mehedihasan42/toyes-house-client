5a5997bb6425b0a2bd100fdfdbf38170e208f07f
mehedi hasan <tanjildewan10@gmail.com>
2023-05-22 20:18:13 +0600

    banner section italic

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold italic">Welcome to <span className='text-fuchsia-500'>ToyesHouse</span></h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="mb-5 italic">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
   </div>
 </div>
