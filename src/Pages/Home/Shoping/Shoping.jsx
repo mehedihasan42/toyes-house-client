fafc23793f56f3cbf4657eb1f29a577bc983e4c2
mehedi hasan <tanjildewan10@gmail.com>
2023-06-14 09:06:05 +0600

    responsive issues

--- src/Pages/Home/Shoping/Shoping.jsx
+++ src/Pages/Home/Shoping/Shoping.jsx
@@ -13,12 +13,12 @@ const Shoping = () => {
     return (
       <div className='my-8'>
        <h2 className='font-bold lg:font-extrabold text-xl bg-slate-300 mx-auto w-36 rounded p-4'>Shop Now</h2>
-      <div className='lg:w-2/3 mx-auto'>
+      <div className='lg:w-2/3 lg:mx-auto'>
         <Tabs>
-    <TabList className="flex flex-center ml-8 lg:w-96 lg:mx-auto my-8 space-x-2 lg:space-x-4">
+    <TabList className="lg:flex lg:flex-center ml-8 w-2/3 lg:w-96 lg:mx-auto my-8 space-x-2 lg:space-x-4">
       <Tab className="px-2 lg:px-4 py-2 bg-gray-700 text-gray-200 rounded-md btn hover:btn-outline cursor-pointer transition-colors duration-300">Cat Toyes</Tab>
       <Tab className="px-2 lg:px-4 py-2 bg-gray-700 text-gray-200 rounded-md btn hover:btn-outline cursor-pointer transition-colors duration-300">Horse Toyes</Tab>
-      <Tab className="px-2 lg:px-4 py-2 bg-gray-700 text-gray-200 rounded-md btn hover:btn-outline cursor-pointer transition-colors duration-300">Taddy Toyes</Tab>
+      <Tab className="px-2 mt-2 lg:mt-0 lg:px-4 py-2 bg-gray-700 text-gray-200 rounded-md btn hover:btn-outline cursor-pointer transition-colors duration-300">Taddy Toyes</Tab>
     </TabList>
 
     <TabPanel>
